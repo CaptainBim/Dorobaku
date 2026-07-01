e8b902e1b3f47a83aab0992d732b10d455f22c46
Sora <enggarekamaulana@gmail.com>
2026-07-01 20:59:16 +0800

    fixed bug on level 5

--- res/scene/hole.gd
+++ res/scene/hole.gd
@@ -4,5 +4,6 @@ extends StaticBody2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if(body.name != "Player"):
+	if(body.name != "Player" and body.name != "layer2"):
+		#print(body.name);
 		body.disappear();
